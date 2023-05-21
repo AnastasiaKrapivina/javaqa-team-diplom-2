44788ef592d2bae1cb348b995acd320d8474a9c3
Anna Belyaeva <anya.belyaeva.1998@list.ru>
2023-05-21 16:41:28 +0300

    Added tests

--- src/test/java/ru/netology/javaqadiplom/BankTest.java
+++ src/test/java/ru/netology/javaqadiplom/BankTest.java
@@ -24,6 +24,15 @@ class BankTest {
         Assertions.assertEquals(2000, account.getTo());
     }
 
+    @Test
+    public void transferFromEqualsAmount() {
+        Bank account = new Bank(4000, 2000);
+        account.transfer(4000);
+
+        Assertions.assertEquals(0, account.getFrom());
+        Assertions.assertEquals(6000, account.getTo());
+    }
+
     @Test // Операция перевода указанной суммы с from на to
     public void transferFromLessToPositiveAmount() {
         Bank account = new Bank(2000, 4000);
@@ -39,6 +48,15 @@ class BankTest {
 
         Assertions.assertEquals(-4000, account.getFrom());
         Assertions.assertEquals(4000, account.getTo());
+    }
+
+    @Test
+    public void transferToNegative() {
+        Bank account = new Bank(4000, -4000);
+        account.transfer(4000);
+
+        Assertions.assertEquals(0, account.getFrom());
+        Assertions.assertEquals(0, account.getTo());
     }
 
 }
